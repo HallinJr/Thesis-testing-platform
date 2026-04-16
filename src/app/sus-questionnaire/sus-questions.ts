c1bd32c1e6df8281af57424e8642233578aec092
alha2310 <alha2310@student.miun.se>
2026-04-16 13:55:16 +0200

    uppdatera sus frågrona

--- src/app/sus-questionnaire/sus-questions.ts
+++ src/app/sus-questionnaire/sus-questions.ts
@@ -6,14 +6,14 @@ export interface SusQuestion {
 }
 
 export const SUS_QUESTIONS: SusQuestion[] = [
-  { key: 'q1', text: 'Jag tror jag skulle vilja använda den här autentiseringsmetoden regelbundet.' },
+  { key: 'q1', text: 'Jag skulle i hög grad kunna tänka mig att använda den här autentiseringsmetoden regelbundet.' },
   { key: 'q2', text: 'Jag upplevde att den här autentiseringsmetoden var onödigt komplicerad.' },
-  { key: 'q3', text: 'Jag tyckte den här autentiseringsmetoden var lätt att använda.' },
-  { key: 'q4', text: 'Jag tror att jag skulle behöva stöd av en tekniskt kunnig person för att kunna använda den här autentiseringsmetoden.' },
-  { key: 'q5', text: 'Jag upplevde att den här autentiseringsmetoden var väl sammanhängande.' },
-  { key: 'q6', text: 'Jag tyckte att denna autentiseringsmetod innehöll för många inkonsekvenser.' },
-  { key: 'q7', text: 'Jag tror att de flesta skulle lära sig att använda den här autentiseringsmetoden mycket snabbt.' },
-  { key: 'q8', text: 'Jag upplevde den här autentiseringsmetoden som besvärlig att använda.' },
+  { key: 'q3', text: 'Jag upplevde att den här autentiseringsmetoden var relativt lätt att använda.' },
+  { key: 'q4', text: 'Jag skulle behöva betydande stöd av en tekniskt kunnig person för att använda den här autentiseringsmetoden.' },
+  { key: 'q5', text: 'Jag upplevde att de olika delarna i autentiseringsmetoden hängde väl ihop.' },
+  { key: 'q6', text: 'Jag upplevde tydliga inkonsekvenser i den här autentiseringsmetoden.' },
+  { key: 'q7', text: 'Jag tror att de flesta relativt snabbt skulle lära sig att använda den här autentiseringsmetoden.' },
+  { key: 'q8', text: 'Jag upplevde den här autentiseringsmetoden som ganska besvärlig att använda.' },
   { key: 'q9', text: 'Jag kände mig trygg i att använda den här autentiseringsmetoden.' },
-  { key: 'q10', text: 'Jag behövde lära mig många saker innan jag kunde använda den här autentiseringsmetoden.' }
+  { key: 'q10', text: 'Jag behövde lära mig mycket innan jag kunde börja använda den här autentiseringsmetoden.' }
 ];
